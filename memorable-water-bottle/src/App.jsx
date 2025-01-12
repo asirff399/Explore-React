e781199760e076fcd74804435f54a8a4fdf1be61
Asir Intisar <aintisar48@gmail.com>
2025-01-12 11:16:50 +0600

    [Simple-Project] - Memorable water bottle

---
+++ memorable-water-bottle/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Bottle from './components/bottle/bottle'
+import Bottles from './components/bottles/Bottles'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Bottles></Bottles>
+      <Bottle></Bottle>
+    </>
+  )
+}
+
+export default App
